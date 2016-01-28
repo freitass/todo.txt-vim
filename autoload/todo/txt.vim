freitass/todo.txt-vim

commit a612ebeaf116d42d058ae78ee28ea5e5c8dd4479

Remove trailing whitespaces

autoload/todo/txt.vim
@@ -41,7 +41,7 @@ endfunction
 function! todo#txt#mark_as_done()
     call s:remove_priority()
     call todo#txt#prepend_date()
-    normal! Ix 
+    normal! Ix
 endfunction
 
 function! todo#txt#mark_all_as_done()

README.markdown
@@ -17,28 +17,28 @@ If your Vim installation does **not** have Python support, this plugin **will wo
 
 This plugin gives syntax highlighting to [todo.txt](http://todotxt.com/) files. It also defines a few mappings, to help with editing these files:
 
-Sorting tasks:  
-`<localleader>s`   Sort the file  
-`<localleader>s+`  Sort the file on +Projects  
-`<localleader>s@`  Sort the file on @Contexts  
-`<localleader>sd`  Sort the file on dates  
-`<localleader>sdd`  Sort the file on due dates  
+Sorting tasks:
+`<localleader>s`   Sort the file
+`<localleader>s+`  Sort the file on +Projects
+`<localleader>s@`  Sort the file on @Contexts
+`<localleader>sd`  Sort the file on dates
+`<localleader>sdd`  Sort the file on due dates
 
-Edit priority:  
-`<localleader>j`   Decrease the priority of the current line  
-`<localleader>k`   Increase the priority of the current line  
-`<localleader>a`   Add the priority (A) to the current line  
-`<localleader>b`   Add the priority (B) to the current line  
-`<localleader>c`   Add the priority (C) to the current line  
+Edit priority:
+`<localleader>j`   Decrease the priority of the current line
+`<localleader>k`   Increase the priority of the current line
+`<localleader>a`   Add the priority (A) to the current line
+`<localleader>b`   Add the priority (B) to the current line
+`<localleader>c`   Add the priority (C) to the current line
 
-Date:  
-`<localleader>d`   Set current task's creation date to the current date  
-`date<tab>`        (Insert mode) Insert the current date  
+Date:
+`<localleader>d`   Set current task's creation date to the current date
+`date<tab>`        (Insert mode) Insert the current date
 
-Mark as done:  
-`<localleader>x`   Mark current task as done  
-`<localleader>X`   Mark all tasks as done  
-`<localleader>D`   Move completed tasks to done.txt  
+Mark as done:
+`<localleader>x`   Mark current task as done
+`<localleader>X`   Mark all tasks as done
+`<localleader>D`   Move completed tasks to done.txt
 
 This plugin detects any text file with the name todo.txt or done.txt with an optional prefix that ends in a period (e.g. second.todo.txt, example.done.txt).
 

syntax/python/dateregex/dateregex/after.py
@@ -20,7 +20,7 @@ def _year_regex_after(year):
             regex = '|' + year[0:idx]
             regex += '9' if digit == '8' else '[%s-9]' % str(int(digit) + 1)
             if idx < len(year) - 1:
-                regex += '\d{%s}' % (len(year) - (idx + 1)) 
+                regex += '\d{%s}' % (len(year) - (idx + 1))
             year_regex += regex
 
     year_regex += ')'
@@ -40,7 +40,7 @@ def _month_regex_after(year, month):
             if digit2 == '8':
                 month_regex = r'(' + month_regex + r'|09)'
             else:
-                month_regex = r'(' + month_regex + r'|0[%s-9])' 
+                month_regex = r'(' + month_regex + r'|0[%s-9])'
                 month_regex = month_regex % str(int(digit2) + 1)
     return '-'.join((year, month_regex, r'\d{2}'))
 

syntax/python/dateregex/dateregex/before.py
@@ -10,7 +10,7 @@
 from datetime import date, timedelta, MINYEAR
 
 def _year_regex_before(year):
-    if int(year) <= MINYEAR: 
+    if int(year) <= MINYEAR:
         return None
     year_regex = r'('
     year_regex += r'\d{1,%s}' % str(len(year) - 1) if len(year) > 1 else ''
@@ -19,12 +19,12 @@ def _year_regex_before(year):
             regex = '|' + year[0:idx]
             regex += '0' if digit == '1' else '[0-%s]' % str(int(digit) - 1)
             if idx < len(year) - 1:
-                regex += '\d{%s}' % (len(year) - (idx + 1)) 
+                regex += '\d{%s}' % (len(year) - (idx + 1))
             year_regex += regex
 
     year_regex += ')'
     return '-'.join((year_regex, r'\d{2}', r'\d{2}'))
-    
+
 def _month_regex_before(year, month):
     if month == '01':
         return None
@@ -33,7 +33,7 @@ def _month_regex_before(year, month):
     if digit1 == '0':
         month_regex = '01' if month == '02' else r'0[1-%s]' % str(int(digit2) - 1)
     elif month == '10':
-        month_regex = r'0\d' 
+        month_regex = r'0\d'
     elif month == '11':
         month_regex = r'(0\d|10)'
     else:
@@ -63,7 +63,7 @@ def _day_regex_before(year, month, day):
     return '-'.join((year, month, day_regex))
 
 
-    
+
 
 def regex_date_before(given_date):
     year, month, day = given_date.isoformat().split('-')

syntax/python/todo.py
@@ -16,11 +16,11 @@ if os.path.isdir(dateregex_dir):
     sys.path.insert(0, dateregex_dir)
 
 def add_due_date_syntax_highlight():
-    try: 
+    try:
         from dateregex import regex_date_before
     except ImportError:
         print("dateregex module not found. Overdue dates won't be highlighted")
-        return 
+        return
 
     regex = regex_date_before(date.today())
     regex = r'(^|<)due:%s(>|$)' % regex

test/tc_sort_context.vim
@@ -1,5 +1,5 @@
 let s:here = expand('<sfile>:p:h')
-let s:tc = unittest#testcase#new('Sort Context', 
+let s:tc = unittest#testcase#new('Sort Context',
       \ { 'data': s:here . '/tc_sort_context.todo.txt' })
 
 let s:LEADER = mapleader

test/tc_sort_date.vim
@@ -1,5 +1,5 @@
 let s:here = expand('<sfile>:p:h')
-let s:tc = unittest#testcase#new('Sort Date', 
+let s:tc = unittest#testcase#new('Sort Date',
       \ { 'data': s:here . '/tc_sort_date.todo.txt' })
 
 let s:LEADER = mapleader

test/tc_sort_project.vim
@@ -1,5 +1,5 @@
 let s:here = expand('<sfile>:p:h')
-let s:tc = unittest#testcase#new('Sort Project', 
+let s:tc = unittest#testcase#new('Sort Project',
       \ { 'data': s:here . '/tc_sort_project.todo.txt' })
 
 let s:LEADER = mapleader

commit 8dd24d0b68924deac1859d816ef14e29b0da4927

Fix wrong prefix for completed tasks
The commit a612ebe ('Remove trailing whitespaces') removed the
whitespace which was after 'x' markup. It caused to mark the
completed task without space and didn't allow to remove it
automatically.

Before this change:
xCOMPLETION_DATE ....
After this change:
x COMPLETION_DATE ...

Fixes: a612ebe ('Remove trailing whitespaces')
Signed-off-by: Leon Romanovsky <leon@leon.nu>

autoload/todo/txt.vim
@@ -41,7 +41,7 @@ endfunction
 function! todo#txt#mark_as_done()
     call s:remove_priority()
     call todo#txt#prepend_date()
-    normal! Ix
+    execute 'normal! Ix '
 endfunction
 
 function! todo#txt#mark_all_as_done()